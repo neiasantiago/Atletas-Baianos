c07c20e95f975281655d54a835185c2df74cda6f
neiasantiago <sneia5392@gmail.com>
2024-09-06 15:22:53 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,32 @@
+let dados = [
+    {
+        titulo : "Isaquias Queiroz",
+        descricao : "Isaquias Queiroz é um dos maiores nomes da canoagem mundial e um orgulho para o esporte brasileiro. Com diversas medalhas olímpicas em seu currículo, incluindo ouro, prata e bronze, ele se tornou um dos atletas mais condecorados do país.",
+        link : "https://pt.wikipedia.org/wiki/Isaquias_Queiroz",
+        tags: "baiano Bahia bahia Brasil brasil",
+    },
+    {
+        titulo : "Rafaelle Souza",
+        descricao : "Rafaelle Souza é uma zagueira central que se destaca tanto pela sua habilidade técnica quanto pela sua liderança dentro de campo. Nascida em Cipó, na Bahia, ela construiu uma carreira sólida no futebol feminino, representando o Brasil em diversas competições internacionais.",
+        link : "https://en.wikipedia.org/wiki/Rafaelle_Souza",
+        tags: "jogadora Brasil",
+    },
+    {
+        titulo : "Beatriz Ferreira",
+        descricao : "Beatriz Ferreira é uma boxeadora brasileira que conquistou o coração dos fãs com sua técnica apurada e espírito de luta. Com uma carreira marcada por grandes feitos, como a medalha de prata nos Jogos Olímpicos de Tóquio, Beatriz se tornou um símbolo do boxe feminino no país.",
+        link : "https://pt.wikipedia.org/wiki/Beatriz_Ferreira",
+        tags: "boxe Bahia bahia Brasil",
+    },
+    {
+        titulo : "Breno Correia",
+        descricao : "Breno Correia é um dos principais nadadores brasileiros da atualidade. Com um estilo de natação elegante e potente, ele conquistou diversas medalhas em competições nacionais e internacionais. Ele é conhecido por sua versatilidade, competindo em diversas provas e sempre buscando superar seus próprios limites.",
+        link : "https://pt.wikipedia.org/wiki/Breno_Correia",
+        tags: "Brasil brasil",
+    },
+    {
+        titulo : "Ana Marcela",
+        descricao : "Ana Marcela Cunha é um dos maiores nomes da natação em águas abertas do mundo e um orgulho para o esporte brasileiro. Com diversas medalhas em campeonatos mundiais e, principalmente, a conquista do ouro olímpico em Tóquio 2020, ela se consolidou como a principal referência da modalidade. Sua resistência, estratégia e habilidade em percorrer longas distâncias em mar aberto a tornaram uma atleta ímpar.",
+        link : "https://pt.wikipedia.org/wiki/Ana_Marcela_Cunha",
+        tags: "Brasil brasil",
+    },
+]
